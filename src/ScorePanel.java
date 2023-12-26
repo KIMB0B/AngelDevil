bc2b59a7b5c500b2fb6229a989533675837582d1
ssarisong <thdtnwls2989@naver.com>
2023-12-26 19:37:05 +0900

    Modify Side Background

--- src/ScorePanel.java
+++ src/ScorePanel.java
@@ -1,28 +1,37 @@
 import java.awt.Color;
-
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import java.awt.Image;
+import java.awt.Graphics;
 
 public class ScorePanel extends JPanel {
 	private int score = 0;
 	private JLabel textLabel = new JLabel("점수");
 	private JLabel scoreLabel = new JLabel(Integer.toString(score));
-	
+	private Image backgroundImage;
+
 	public ScorePanel() {
-		this.setBackground(Color.YELLOW);
+		backgroundImage = new ImageIcon("./resources/side.png").getImage();
+		//this.setBackground(Color.YELLOW);
 		setLayout(null);
-		
+
 		textLabel.setSize(50, 20);
 		textLabel.setLocation(10, 10);
 		add(textLabel);
-		
+
 		scoreLabel.setSize(100, 20);
 		scoreLabel.setLocation(70, 10);
-		add(scoreLabel);		
+		add(scoreLabel);
 	}
-	
+
 	public void increase() {
 		score += 10;
 		scoreLabel.setText(Integer.toString(score));
+	}
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
 	}
 }
