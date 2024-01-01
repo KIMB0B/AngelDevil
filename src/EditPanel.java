da5e7e78d6f3e6399b90c28f555cbd18739c6e2b
ssarisong <thdtnwls2989@naver.com>
2024-01-01 21:53:37 +0900

    add edit background image

--- src/EditPanel.java
+++ src/EditPanel.java
@@ -1,20 +1,28 @@
-import java.awt.Color;
 import java.awt.FlowLayout;
-
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
 public class EditPanel extends JPanel {
 	private JTextField edit = new JTextField(20);
-	private JButton addButton = new JButton("add");
-	private JButton saveButton = new JButton("save");
+	private JButton addButton = new JButton("단어 추가");
+	private JButton saveButton = new JButton("단어 저장");
+	private Image backgroundImage;
 	
 	public EditPanel() {
-		this.setBackground(Color.YELLOW);
 		this.setLayout(new FlowLayout());
 		add(edit);
 		add(addButton);
 		add(saveButton);
+
+		backgroundImage = new ImageIcon("./resources/edit.png").getImage();
+	}
+
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
 	}
 }
